6741b8bca20de50d854eaf5dfafd6061103e44ef
Eddy Chen <niiq103@hotmail.com>
2019-10-11 20:30:11 +0800

    Initial Commit

---
+++ remember my name/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  remember my name
+//
+//  Created by Eddy Chen on 2019/10/11.
+//  Copyright © 2019 Eddy Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
